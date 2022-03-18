6a7b98b555920498fa993fc7687d59119f12b371
NASZILLA247 <nasirulord@gmail.com>
2022-03-18 05:15:27 -0700

    files added

---
+++ js/lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": 'English',
+        "location": 'London',
+        "price": 5000,
+        "image": "https://img.icons8.com/ios/480/000000/circled-e.png",
+        "available": 5
+
+    },
+    {
+        "id": 1002,
+        "subject": 'Music',
+        "location": 'Oxford',
+        "price": 4000,
+        "image": "https://img.icons8.com/windows/480/000000/apple-music.png",
+        "available": 9
+
+    },
+    {
+        "id": 1003,
+        "subject": 'Music',
+        "location": 'York',
+        "price": 7000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 11
+
+    },
+    {
+        "id": 1004,
+        "subject": 'English',
+        "location": 'Bristol',
+        "price": 2000,
+        "image": "https://img.icons8.com/ios/480/000000/circled-e.png",
+        "available": 8
+
+    },
+    {
+        "id": 1005,
+        "subject": 'Maths',
+        "location": 'London',
+        "price": 5000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 10
+
+    },
+    {
+        "id": 1006,
+        "subject": 'Music',
+        "location": 'Oxford',
+        "price": 5000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 10
+
+    },
+    {
+        "id": 1007,
+        "subject": 'Maths',
+        "location": 'Oxford',
+        "price": 5000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 10
+
+    },
+    {
+        "id": 1008,
+        "subject": 'Maths',
+        "location": 'London',
+        "price": 5000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 10
+
+    },
+    {
+        "id": 1009,
+        "subject": 'Maths',
+        "location": 'Dubai',
+        "price": 10000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 10
+
+    },
+    {
+        "id": 1010,
+        "subject": 'English',
+        "location": 'York',
+        "price": 7000,
+        "image": "https://img.icons8.com/external-outline-juicy-fish/480/000000/external-maths-school-outline-outline-juicy-fish.png",
+        "available": 10
+
+    },
+]
